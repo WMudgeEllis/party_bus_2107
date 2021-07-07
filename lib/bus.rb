a691e98a3fe2cd1c03dc65ed815c4193c5d1ce9d
Weston Ellis <ellisweston112@gmail.com>
2021-07-07 14:46:45 -0600

    Finish iteration2

--- lib/bus.rb
+++ lib/bus.rb
@@ -7,6 +7,18 @@ def initialize(name, capacity)
   @passengers = []
 end
 
+def add_passenger(pass_name)
+  @passengers << pass_name
+end
 
+def yell_at_passengers
+    yelled_at_passengers = []
+
+    @passengers.each do |rider|
+      yelled_at_passengers << rider.upcase
+    end
+
+    yelled_at_passengers
+end
 
 end
